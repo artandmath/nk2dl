213bf26159449a83839d26afc70189781ce19d66
Daniel H. <danielharkness@mac.com>
2025-07-30 09:45:46 +1000

    Remove migration directory from tracking

--- migration/nk2dl/__main__.py
+++
@@ -1,9 +0,0 @@
-"""Main entry point for running nk2dl as a module.
-
-This allows running the package with 'python -m nk2dl'.
-"""
-
-from . import cli_main
-
-if __name__ == "__main__":
-    cli_main() 
