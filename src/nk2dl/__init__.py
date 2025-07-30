df91d48df61e56bec1794e8f7fab6be091e03b90
Daniel H. <danielharkness@mac.com>
2025-07-30 11:03:27 +1000

    update copyright output

--- src/nk2dl/__init__.py
+++ src/nk2dl/__init__.py
@@ -4,17 +4,20 @@ Nuke to Deadline Submitter - Core Library
 A pure Python library for submitting Nuke scripts to Thinkbox Deadline.
 """
 
-print("\nNuke to Deadline (nk2dl) v0.1")
-print("Copyright (c) 2025 Daniel Harkness. All Rights Reserved.\n")
+from datetime import date
+
+__version__ = "0.1.0"
+__author__ = "Daniel Harkness"
+__email__ = "danielharkness@icloud.com"
+
+print(f"\nNuke to Deadline (nk2dl) v{__version__}")
+print(f"Copyright (c) {date.today().year} {__author__}. All Rights Reserved.\n")
 
 from .api import submit_nuke_script
 from .config import config
 from .errors import NK2DLError, SubmissionError, ConfigError
 from .connection import DeadlineConnection
 
-__version__ = "0.1.0"
-__author__ = "Daniel Harkness"
-__email__ = "danielharkness@icloud.com"
 
 __all__ = [
     "submit_nuke_script",
